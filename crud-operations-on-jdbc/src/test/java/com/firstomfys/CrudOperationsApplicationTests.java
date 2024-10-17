2bbe1fed7e0f94c12b8fe69e65809adfaf76b9c4
CharuliShirsath <141058927+CharuliShirsath@users.noreply.github.com>
2024-10-17 18:06:44 +0530

    Add files via upload

---
+++ crud-operations-on-jdbc/src/test/java/com/firstomfys/CrudOperationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firstomfys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudOperationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
